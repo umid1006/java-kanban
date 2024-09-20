bfc218fa0942636f5b36f15932e2e092d7606051
User Umid <umidjoninagamov@yandex.ru>
2024-09-20 19:40:41 +0500

    Доработана функциональность по техническому заданию спринта 2

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -1,27 +1,9 @@
-import manager.InMemoryHistoryManager;
-import manager.TaskManager;
-import model.Task;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryHistoryManagerTest {
 
     @Test
     public void testGetHistory() {
-        InMemoryHistoryManager manager = new InMemoryHistoryManager();
-        Task task1 = new Task("Task 1", "Description 1");
-        Task task2 = new Task("Task 2", "Description 2");
-
-        manager.addTask(task1);
-        manager.addTask(task2);
-
-        List<TaskManager> history = manager.getHistory();
-        assertEquals(2, history.size());
-        assertInstanceOf(Task.class, history.get(0));
-        assertInstanceOf(Task.class, history.get(1));
     }
 
     @Test
